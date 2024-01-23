50c61bf469af64139c62672360aeda5d759262f1
iconicstixx <ccalvinoutlookcom@gmail.com>
2024-01-23 11:26:46 +0800

    fix: trying to get rid of a nonexistent problem on my end

--- LeaderBoard.py
+++ LeaderBoard.py
@@ -4,11 +4,21 @@ LD = f.readlines()
 
 # Cleans up list
 for i in range(len(LD)):
-    LD[i] = int(LD[i].strip())
+    LD[i] = LD[i].strip()
 
 # Current List = ['00000', '00000', '00000', '00000', '00000']
-
+def update_score_list(score):
+    current_score = score.score
+    for i in range(len(LD)):
+        if current_score > LD[i]:
+            j = len(LD) - 1
+            while j > i:
+                LD[j] = LD[j - 1]
+                j -= 1
+            LD[i] = current_score
+            break  # Exit the loop
 # Records current score
 
 f = open("Leaderboard.txt", "w")
-f.write(str(LD) + "\n")
+for item in LD:
+    f.write(item + "\n")
