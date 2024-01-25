0f759742641ed27530185f16a83744e9f6e6e509
iconicstixx <ccalvinoutlookcom@gmail.com>
2024-01-25 14:04:57 +0800

    fix: slight adjustment to how i worded the output.

--- highest_score.py
+++ highest_score.py
@@ -19,15 +19,15 @@ existing_highscore_user = LB[0]
 existing_highest_score = int(LB[1])
 
 #takes an input through terminal for new user session
-new_user = input(str("Enter your 6 letter initials: "))
+new_user = input("Enter your name: ")
 
-#function to call settings and score attribute
+    #function to call settings and score attribute
 def update_score_list(settings, score):
     # If the game is not running and the score has not been added to the leaderboard
     if not settings.running and not score.added_to_leaderboard:
         current_score = score.score
         score.added_to_leaderboard = True  # Set a flag so it won't append the score again
-
+        #compares the current score to the previous highscore
         if current_score > existing_highest_score:
             # writes the new user and new score into the file
             f = open("highest_score_file.txt", "w")
@@ -36,8 +36,8 @@ def update_score_list(settings, score):
 #this function is an iteration of the show_game_over function
 def show_previous_highest_score(settings, screen):
     #  Font settings
-     text_color = (255, 255, 255)
-     font = pygame.font.SysFont(None,38)
-     showgameover = font.render( "Highest Score: " + str(existing_highest_score), True, text_color, settings.bg_color)
-     dest=(57,66)
-     screen.blit(showgameover,dest)
+    text_color = (255, 255, 255)
+    font = pygame.font.SysFont(None,38)
+    showgameover = font.render( "Highest Score: " + str(existing_highest_score), True, text_color, settings.bg_color)
+    dest=(57,66)
+    screen.blit(showgameover,dest)
